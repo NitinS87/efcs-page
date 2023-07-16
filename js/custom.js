8aadc4a1d07aceeba97e59014bef4b05237a4355
Nitin Sharma <nitin.sharma@coffeee.io>
2023-07-16 10:26:00 +0530

    Created EFCS page

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+/*
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+function togglePriceInfo() {
+  var priceInfo = document.querySelector(".price-info");
+  if (priceInfo.style.display === "none") {
+    priceInfo.style.display = "flex";
+  } else {
+    priceInfo.style.display = "none";
+  }
+}
+ */
